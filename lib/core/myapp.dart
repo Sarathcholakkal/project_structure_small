e20a47197dfe3259d08d336bd2f0da08d3f71d07
sarath cholakkal <ssarathcholakkall@gmail.com>
2024-02-29 18:28:17 +0530

    basic structure added

---
+++ lib/core/myapp.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+import '../presentation/screens/counter_screen/home.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomeScreen(),
+    );
+  }
+}
